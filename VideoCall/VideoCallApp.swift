8a21bd9a44383d5e42400dfd5af5524b6970c8e3
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-22 16:46:20 +0300

    VideoCall App was completed

--- VideoCall/VideoCallApp.swift
+++ VideoCall/VideoCallApp.swift
@@ -1,17 +1,123 @@
-//
-//  VideoCallApp.swift
-//  VideoCall
-//
-//  Created by ahmet karadağ on 22.10.2024.
-//
-
-import SwiftUI
+/*import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
 
 @main
 struct VideoCallApp: App {
+    @State var call: Call
+    @ObservedObject var state: CallState
+    @State var callCreated: Bool = false
+
+    private var client: StreamVideo
+
+    private let apiKey: String = ""
+    private let token: String = ""
+    private let userId: String = ""
+    private let callId: String = ""
+
+    init() {
+        let user = User(
+            id: userId,
+            name: "Martin", // name and imageURL are used in the UI
+            imageURL: .init(string: "https://getstream.io/static/2796a305dd07651fcceb4721a94f4505/a3911/martin-mitrevski.webp")
+        )
+
+        // Initialize Stream Video client
+        self.client = StreamVideo(
+            apiKey: apiKey,
+            user: user,
+            token: .init(stringLiteral: token)
+        )
+
+        // Initialize the call object
+        let call = client.call(callType: "default", callId: callId)
+
+        self.call = call
+        self.state = call.state
+    }
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            VStack {
+                if callCreated {
+                    ZStack {
+                        ParticipantsView(
+                            call: call,
+                            participants: call.state.remoteParticipants,
+                            onChangeTrackVisibility: changeTrackVisibility(_:isVisible:)
+                        )
+                        FloatingParticipantView(participant: call.state.localParticipant)
+                    }
+                } else {
+                    Text("loading...")
+                }
+            }.onAppear {
+                Task {
+                    guard callCreated == false else { return }
+                    try await call.join(create: true)
+                    callCreated = true
+                }
+            }
+        }
+    }
+
+    /// Changes the track visibility for a participant (not visible if they go off-screen).
+    /// - Parameters:
+    ///  - participant: the participant whose track visibility would be changed.
+    ///  - isVisible: whether the track should be visible.
+    private func changeTrackVisibility(_ participant: CallParticipant?, isVisible: Bool) {
+        guard let participant else { return }
+        Task {
+            await call.changeTrackVisibility(for: participant, isVisible: isVisible)
+        }
+    }
+}*/
+import SwiftUI
+import StreamVideo
+import StreamVideoSwiftUI
+
+@main
+struct VideoCallApp: App {
+    @ObservedObject var viewModel: CallViewModel
+
+    private var client: StreamVideo
+
+    private let apiKey: String = ""
+    private let token: String = ""
+    private let userId: String = ""
+    private let callId: String = ""
+
+    init() {
+        let user = User(
+            id: userId,
+            name: "Martin", // name and imageURL are used in the UI
+            imageURL: .init(string: "https://getstream.io/static/2796a305dd07651fcceb4721a94f4505/a3911/martin-mitrevski.webp")
+        )
+
+        // Initialize Stream Video client
+        self.client = StreamVideo(
+            apiKey: apiKey,
+            user: user,
+            token: .init(stringLiteral: token)
+        )
+
+        self.viewModel = .init()
+    }
+
+    var body: some Scene {
+        WindowGroup {
+            VStack {
+                if viewModel.call != nil {
+                    CallContainer(viewFactory: DefaultViewFactory.shared, viewModel: viewModel)
+                } else {
+                    Text("loading...")
+                }
+            }.onAppear {
+                Task {
+                    guard viewModel.call == nil else { return }
+                    viewModel.joinCall(callType: .default, callId: callId)
+                }
+            }
         }
     }
 }

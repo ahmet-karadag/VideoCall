b0146b1dfd4cf6454a315612e43f7f14da64ffba
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-22 11:49:52 +0300

    Initial Commit

---
+++ VideoCall/VideoCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoCallApp.swift
+//  VideoCall
+//
+//  Created by ahmet karadağ on 22.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
